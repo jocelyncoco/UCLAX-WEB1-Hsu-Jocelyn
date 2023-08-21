9fba1a9afc4df9d1a5a989b4eae0331aed922904
Jocelyn Hsu <jhsu822@gmail.com>
2023-08-20 23:56:41 -0700

    location added

---
+++ src/Pages/Primary/Location/Location.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const location = () => {
+    return <locationStyled className="location">location</locationStyled>;
+};
+
+export default location;
+
+const locationStyled = styled.div``;
